225fceb449e43638187a68552cdf4b5702fe91cc
Isuru Weerasooriya <idweerasooriya@gmail.com>
2014-02-08 20:32:36 -0500

    Finished coding buildarff.py

--- buildarff.py
+++ buildarff.py
@@ -2,30 +2,42 @@ import sys
 import re
 
 # Globals
-number_of_rlines = -1
+master_output = []
 class_names = {}
-# class name is constructed so that the classes are the keys and their corresponding files
-# files are its values (which are stored in a list)
+# Classes are the keys and their corresponding files
+# are its values (which are stored in a list).
 imported_features = []
 other_features = ["upper_case", "avg_len_token", "avg_len_sentence", "num_sentence"]
-
-master_output = []
 # The order in which features will be attached are:
 # fp_pronoun, sp_pronoun, tp_pronoun, coordinat_conj, past_tense_verbs,
 # future_tense_verbs, commas, colons, dash, parentheses, ellipses,
 # common_nouns, proper_nouns, adverbs, wh-words, modern_slang,
 # upper_case, avg_len_token, avg_len_sentence, num_sentence
 
+'''
+	MAIN - Runs the program.
+	- Parses parameters
+	- Gather features
+	- Parses tweet files for f
+'''
 def main():
-	output_f = parse_args(sys.argv[1:])
+	output_file, number_of_twt = parse_args(sys.argv[1:])
 	get_features("features")
-	get_lines()
-	write_output(output_f)
+	parse_features(number_of_twt)
+	write_output(output_file)
 
-	# Next we want to get all the features
-
+'''
+	Actually writes the output_file arff file, using
+	master_output. Both feature globals and the class_names
+	are also used.
+'''
 def write_output(fname):
-	print fname
+	# globals used
+	global class_names
+	global master_output
+	global imported_features
+	global other_features
+
 	try:
 		fw = open(fname, 'w')
 	except:
@@ -35,16 +47,16 @@ def write_output(fname):
 	fw.write("@RELATION TWEET \n\n")
 
 	for feature in imported_features:
-		fw.write("@ATTRIBUTE " + feature[0] + " NUMERIC\n")
+		fw.write("@ATTRIBUTE " + feature[0] + "\t\tNUMERIC\n")
 
 	for feature in other_features:
-		fw.write("@ATTRIBUTE " + feature + " NUMERIC\n")
+		fw.write("@ATTRIBUTE " + feature + "\t\tNUMERIC\n")
 
-	fw.write("@ATTRIBUTE class {")
+	fw.write("@ATTRIBUTE class\t\t{")
 	cn = class_names.keys()
 	for i in range(len(cn)):
 		if i != len(cn) - 1:
-			fw.write(cn[i] + ",")
+			fw.write(cn[i] + ", ")
 		else:
 			fw.write(cn[i])
 	fw.write("}\n\n")
@@ -54,19 +66,24 @@ def write_output(fname):
 	for tmp_lst in master_output:
 		for i in range(len(tmp_lst)):
 			if i != 20:
-				fw.write(tmp_lst[i] + ",")
+				fw.write(tmp_lst[i] + ", ")
 			else:
 				fw.write(tmp_lst[i])
 		fw.write("\n")
 
+'''
+	Calculates the number of features for each tweet. As well as
+	the upper_case feature and the avg_len_token featre. Returns
+	all this data in the a list.
+'''
 def process_tweet(tweet_info):
-
-	# This value will be calculated by the process_tweet function
-	# as it is calculating all the values for imported_features.
+	# The length of all tokens in the tweet
 	total_token_len = 0
+	# The number of tokens in the tweet
 	number_of_tokens = 0
-
+	# Number of capitalized words
 	upper_case = 0
+
 	tmp_output_data = []
 	counter = 0
 
@@ -74,10 +91,11 @@ def process_tweet(tweet_info):
 		tmp_tk = re.split(r"/", token)
 
 		if re.match(r"[A-Z][A-Z]+", tmp_tk[0]):
+			# upper_case calculation
 			upper_case = upper_case + 1
 
 		if not re.match(r"[\W]+", tmp_tk[0]):
-			#calculates everything needed to calculate average token  length
+			# avg_len_token calculation
 			number_of_tokens = number_of_tokens + 1
 			total_token_len = total_token_len + len(token[0])
 
@@ -92,16 +110,27 @@ def process_tweet(tweet_info):
 			elif imported_features[i][1] == "tags":
 				if tmp_tk[1] in imported_features[i][2]:
 					counter = counter + 1
+		# Appends imported_features to the master_output
 		tmp_output_data.append(str(counter))
 
+	# Appends other_features to the master_output
 	tmp_output_data.append(str(upper_case))
-	# this is the average token length
-	tmp_output_data.append(str(total_token_len/number_of_tokens))
+	if number_of_tokens != 0:
+                tmp_output_data.append(str(total_token_len/number_of_tokens))
+        else:
+                tmp_output_data.append("0")
 
 	return tmp_output_data
 
-def get_lines():
-	#if number_of_rlines == -1:
+'''
+	Parses the appropriate tweet files for feature according to 
+	class_names, and then construcs a master_output of all the
+	features.
+'''
+def parse_features(number_of_twt):
+	global class_names
+	global master_output
+
 	for cn in class_names.keys():
 		list_of_files = class_names[cn]
 		for f in list_of_files:
@@ -111,43 +140,34 @@ def get_lines():
 				sys.stderr.write("Error while reading file: " + f + "\n")
 				continue
 			
+			# Keeps track how the number of tweets
 			tweet_count = 0
-			# Keeps track of the tweets processed.
-
+			# Keeps track of the number of sentences
 			num_sentence = 0
-			# A simple counter as we move from one sentence to the next
-			# in a tweet.
-
-			sentence_len = 0
-			# avg_len_sentence will be calculated by adding up all the
-			# numbers in sentence_len_list and diving that number by the
-			# num_sentence value.
-
+			# Keeps track of the length of the tweet
+			twt_len = 0
+			# The tokens/tags for each tweet
 			tweet_info = []
-			# Will store all the tokens and their tags for each tweet and
-			# will be passed into process_tweet.
 
 			for line in fr:
 				if line.strip() != '|':
 					m_list = re.split(r" ", line)
 					m_list.remove("\n")
 					
+					# Update feature information
 					num_sentence = num_sentence + 1
-					sentence_len = sentence_len + len(m_list)
-					
+					twt_len = twt_len + len(m_list)
 					tweet_info = tweet_info + m_list					
 				else:
 					tmp_output_data = process_tweet(tweet_info)
-					# This will return the average length of tokens
-					# retreve al the information and store it somewhere.
-					
-					# This is where we want to add all the information
 
-					# This is the average sentece length
-					tmp_output_data.append(str(sentence_len/num_sentence))
-					# This is the number of sentences per tweet
+					# Appends other_features to the master_output
+					if num_sentence != 0:
+						tmp_output_data.append(str(twt_len/num_sentence))
+						# Calculate avg_len_sentence
+					else:
+						tmp_output_data.append("0")
 					tmp_output_data.append(str(num_sentence))
-					# This is the class name being attached
 					tmp_output_data.append(cn)
 
 					# This is the final output to the arff file
@@ -158,11 +178,16 @@ def get_lines():
 					tweet_info = []
 
 					tweet_count = tweet_count + 1
-					if tweet_count == number_of_rlines:
+					if str(tweet_count) == str(number_of_twt):
 						# This is for when the number of tweets to read
 						# is specified.
 						break
 
+'''
+	Reads in all features and all words and according tags for 
+	those features. imported_features will store all this information and 
+	will be used to 
+'''
 def get_features(fname):
 	try:
 		fr = open(fname, 'r')
@@ -172,40 +197,51 @@ def get_features(fname):
 
 	for line in fr:
 		tmp = eval(line)
+		global imported_features # global used
 		imported_features.append([tmp[0], tmp[1], tmp[2:]])
-			#tmp 1 has the feature name
-			# tmp 0 has if its a word tag or both
-			# tmo 2 and onwards has all the tags associated with it
-	#print imported_features
+		# The format of imported_features is as follows:
+		# tmp[0] - feature name
+		# tmp[1] - type of the feature - words / tags
+		# tmp[2:] - how to identify the feature
 
+'''
+	Parses the argument list and extracts the following:
+	- Number of tweets per file, to be read
+	- Classes and theiry associated files - global class_names
+	- Output file, where the arff file will be written
+'''
 def parse_args(args):
+	number = 0
 	if len(args) > 1:
-		output_f = args[len(args) - 1]
-
-		m_one = re.search(r"-(\d+)", args[0])
-		i = 1
-		if m_one:
-			number_of_rlines = m_one.group(1)
-			i = 2
+		output_file = args[len(args) - 1]
 		
-		if i >= len(args):
+		m_one = re.search(r"-(\d+)", args[0])
+		param_num = 1
+		if m_one:
+			# Specification of a certain set of tweets
+			number = m_one.group(1)
+			param_num = 2
+		
+		if param_num >= len(args):
 			sys.stderr.write("Not enough parameters provided!\n")
 
-		for j in range(0, len(args) - 1):
-			m_two = re.split(r":", args[j])
+		global class_names # global used
+		for i in range(len(args)):
+			m_two = re.split(r":", args[i])
 			if len(m_two) == 1:
+				# This is for the case: file1.twt
 				m_three = re.search(r"(\w+).twt", m_two[0])
 				if m_three:
 					class_names[m_three.group(1)] = [m_two[0]]
 			else:
-				# If we get here its the case that m_two = [pop, ""]
+				# This is for the case: class:file1.twt+file2.twt
 				m_four = re.split(r"\+", m_two[1])
 				if len(m_four) > 0:
 					class_names[m_two[0]] = m_four
-		return output_f
+
+		return (output_file, number)
 	else:
 		sys.stderr.write("Not enough parameters provided!\n")
 
-#[x for x in list1 if x in list2]
 if __name__ == "__main__":
 	main()
